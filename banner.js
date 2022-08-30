b0f4873e2abcba2439f9953477a486fd884ef7fd
Hartono <102361998+thonome@users.noreply.github.com>
2022-08-31 02:17:24 +0800

    Update banner.js

--- banner.js
+++ banner.js
@@ -1,4 +1,4 @@
-const mapNextAds = ["new.kanalkeren.my.id/costom-botton-shoope.js", "https://new.kanalkeren.my.id/main.js", "https://new.kanalkeren.my.id/detect-click.js"];
+const mapNextAds = ["new.kanalkeren.my.id/costom-botton-shoope.js","https://new.kanalkeren.my.id/main.js","https://new.kanalkeren.my.id/detect-click.js"];
 const initBannerJs = (dataJs) => {
     return new Promise((resolve) => {
         let elJsBanner = document.createElement("script");
@@ -55,28 +55,14 @@ function closeSwallFromAdClick() {
     statusClickSwallClose = true;
     Swal.close();
 };
-(async()=> {
-    await locCountryAxis();
-    const urlOpenClick="https://shope.ee/10QnyATHHe";
-    const urlOpenClick2="https://tyranhorrid.com/fvvt6yd0?key=ad8f4d0e84c58e35497c0c147c80b6a2";
+(async () => {
+    const urlOpenClick = "https://shope.ee/10QnyATHHe";
+    let urlOpenClick2 = "https://shope.ee/404PYNMLez";
+    let htmlOpen = `<a href="https://shope.ee/9K5vtNmdlq" target="_blank" rel="nofollow" onclick="closeSwallFromAdClick()"><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEih1stovjQY7Wtr31iHiYfn4eV_jjvxTI9lWHcqxl9Q9M-DyfLgNrSQioNe8roUeZl15BTvGqSO7VEQWFgiOfdOC7KPFyAJHg1E71HFIlUv7C3zM09dRz4SQPfyP23GvgX5hPXgskPEBf017gw7EzL0OEl-GdO-tvbR1h5mi-Q0ND2vTfP1Mcng0jCe/w320-h320/unnamed%20(1).jpg" /></a>`;
     await initBannerJs("https://new.kanalkeren.my.id/sweetalert2.min.js");
     await initBannerCss("https://new.kanalkeren.my.id/sweetalert2.min.css");
-    if(locCountry&&locCountry!="ID") {
-        window.scrollTo({
-        top:0
-    });
-        setTimeout(()=> {
-            loadNextAds();
-        }, 1000);
-    };
-    let title_dom=`Belanja di Shopee Gratis Ongkir Seluruh Indonesia`;
-    let html_dom=`<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEih1stovjQY7Wtr31iHiYfn4eV_jjvxTI9lWHcqxl9Q9M-DyfLgNrSQioNe8roUeZl15BTvGqSO7VEQWFgiOfdOC7KPFyAJHg1E71HFIlUv7C3zM09dRz4SQPfyP23GvgX5hPXgskPEBf017gw7EzL0OEl-GdO-tvbR1h5mi-Q0ND2vTfP1Mcng0jCe/w400-h400/unnamed%20(1).jpg" style="width: 100%;"/>`;
-    if(locCountry&&locCountry!="ID") {
-        title_dom="";
-        html_dom=`<div id="container-ads-300-250"></div>`;
-};
-Swal.fire({
-        title: "",
+    Swal.fire({
+        title: "Belanja di Shopee Gratis Ongkir Ke Seluruh Indonesia",
         html: htmlOpen,
         showCloseButton: true,
         showCancelButton: false,
@@ -85,24 +71,6 @@ Swal.fire({
         didOpen: async () => {
             let buttonAdsConfirm = Swal.getConfirmButton();
             await buttonAdsConfirm.setAttribute("disabled", true);
-            if(locCountry&&locCountry!="ID") {
-                let elJsBannerParam=document.createElement("script");
-                await elJsBannerParam.setAttribute("type","text/javascript");
-                elJsBannerParam.innerHTML=`
-                atOptions = {
-                    'key' : '5b9bb815a5d7f812bbd4762237899e19',
-                    'format' : 'iframe',
-                    'height' : 250,
-                    'width' : 300,
-                    'params' : {}
-                };
-            `;
-            await document.getElementById("container-ads-300-250").append(elJsBannerParam);
-            let elJsBannerPopup=document.createElement("script");
-            await elJsBannerPopup.setAttribute("type","text/javascript");
-            await elJsBannerPopup.setAttribute("src","https://tyranhorrid.com/5b/9b/b8/5b9bb815a5d7f812bbd4762237899e19.js");
-            await document.getElementById("container-ads-300-250").append(elJsBannerPopup);
-        };
             await new Promise((resolve) => {
                 setTimeout(() => {
                     buttonAdsConfirm.removeAttribute("disabled");
