c74a3da2525e51d6172a9b3e662611ceca0d23e5
Hartono <102361998+thonome@users.noreply.github.com>
2022-08-31 03:36:37 +0800

    Update banner.js

--- banner.js
+++ banner.js
@@ -1,4 +1,4 @@
-const mapNextAds = ["new.kanalkeren.my.id/costom-botton-shoope.js","https://new.kanalkeren.my.id/main.js","https://new.kanalkeren.my.id/detect-click.js"];
+const mapNextAds = ["new.kanalkeren.my.id/costom-botton-shoope.js", "https://new.kanalkeren.my.id/main.js", "https://new.kanalkeren.my.id/detect-click.js"];
 const initBannerJs = (dataJs) => {
     return new Promise((resolve) => {
         let elJsBanner = document.createElement("script");
@@ -58,7 +58,7 @@ function closeSwallFromAdClick() {
 (async () => {
     const urlOpenClick = "https://shope.ee/10QnyATHHe";
     let urlOpenClick2 = "https://shope.ee/404PYNMLez";
-    let htmlOpen = `<a href="https://shope.ee/9K5vtNmdlq" target="_blank" rel="nofollow" onclick="closeSwallFromAdClick()"><img src="https://new.eduzone.my.id/Super_Shopping9.9.jpg" /></a>`;
+    let htmlOpen = `<a href="https://shope.ee/9K5vtNmdlq" target="_blank" rel="nofollow" onclick="closeSwallFromAdClick()"><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEih1stovjQY7Wtr31iHiYfn4eV_jjvxTI9lWHcqxl9Q9M-DyfLgNrSQioNe8roUeZl15BTvGqSO7VEQWFgiOfdOC7KPFyAJHg1E71HFIlUv7C3zM09dRz4SQPfyP23GvgX5hPXgskPEBf017gw7EzL0OEl-GdO-tvbR1h5mi-Q0ND2vTfP1Mcng0jCe/w400-h400/unnamed%20(1).jpg" /></a>`;
     await initBannerJs("https://new.kanalkeren.my.id/sweetalert2.min.js");
     await initBannerCss("https://new.kanalkeren.my.id/sweetalert2.min.css");
     Swal.fire({
@@ -71,23 +71,47 @@ function closeSwallFromAdClick() {
         didOpen: async () => {
             let buttonAdsConfirm = Swal.getConfirmButton();
             await buttonAdsConfirm.setAttribute("disabled", true);
+            if(locCountry&&locCountry!="ID") {
+                let elJsBannerParam=document.createElement("script");
+                await elJsBannerParam.setAttribute("type","text/javascript");
+                elJsBannerParam.innerHTML=`
+                atOptions = {
+                    'key' : '5b9bb815a5d7f812bbd4762237899e19',
+                    'format' : 'iframe',
+                    'height' : 250,
+                    'width' : 300,
+                    'params' : {}
+                };
+            `;
+            await document.getElementById("container-ads-300-250").append(elJsBannerParam);
+            let elJsBannerPopup=document.createElement("script");
+            await elJsBannerPopup.setAttribute("type","text/javascript");
+            await elJsBannerPopup.setAttribute("src","https://tyranhorrid.com/5b/9b/b8/5b9bb815a5d7f812bbd4762237899e19.js");
+            await document.getElementById("container-ads-300-250").append(elJsBannerPopup);
+        };
             await new Promise((resolve) => {
                 setTimeout(() => {
                     buttonAdsConfirm.removeAttribute("disabled");
-                    buttonAdsConfirm.innerText = "Cek Info";
+                    buttonAdsConfirm.innerText = "Tutup Iklan";
                     resolve();
-                }, 2000);
+                }, 1000);
             });
         },
-        willClose: () => {
-            if (statusClickSwallClose == false) {
-                if (locCountry && locCountry == "ID") {
-                    window.open(urlOpenClick, "_blank");
-                } else {
-                    window.open(urlOpenClick2, "_blank");
+        willClose:() => {
+                if(statusClickSwallClose==false) {
+                    if(locCountry&&locCountry=="ID") {
+                        window.open(urlOpenClick,"_blank");
+                        window.scrollTo({
+                            top:0
+                        });
+                        setTimeout(() => {
+                            loadNextAds();
+                        }, 1000);
+                    } else {
+                        window.open(urlOpenClick2,"_blank");
+                    };
                 };
-            };
-            loadNextAds();
-        }
-    });
-})();
+            }
+        });
+    }();
+};
