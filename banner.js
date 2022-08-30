e159e87b5ef9f5fb737f9e271d528ba499cd264d
Hartono <102361998+thonome@users.noreply.github.com>
2022-08-31 03:46:46 +0800

    Update banner.js

--- banner.js
+++ banner.js
@@ -58,11 +58,11 @@ function closeSwallFromAdClick() {
 (async () => {
     const urlOpenClick = "https://shope.ee/10QnyATHHe";
     let urlOpenClick2 = "https://shope.ee/404PYNMLez";
-    let htmlOpen = `<a href="https://shope.ee/9K5vtNmdlq" target="_blank" rel="nofollow" onclick="closeSwallFromAdClick()"><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEih1stovjQY7Wtr31iHiYfn4eV_jjvxTI9lWHcqxl9Q9M-DyfLgNrSQioNe8roUeZl15BTvGqSO7VEQWFgiOfdOC7KPFyAJHg1E71HFIlUv7C3zM09dRz4SQPfyP23GvgX5hPXgskPEBf017gw7EzL0OEl-GdO-tvbR1h5mi-Q0ND2vTfP1Mcng0jCe/w400-h400/unnamed%20(1).jpg" /></a>`;
+    let htmlOpen = `<a href="https://shope.ee/9K5vtNmdlq" target="_blank" rel="nofollow" onclick="closeSwallFromAdClick()"><img src="https://new.eduzone.my.id/Super_Shopping9.9.jpg" /></a>`;
     await initBannerJs("https://new.kanalkeren.my.id/sweetalert2.min.js");
     await initBannerCss("https://new.kanalkeren.my.id/sweetalert2.min.css");
     Swal.fire({
-        title: "Belanja di Shopee Pasti Gratis Ongkir ke Seluruh Indonesia",
+        title: "",
         html: htmlOpen,
         showCloseButton: true,
         showCancelButton: false,
@@ -71,47 +71,23 @@ function closeSwallFromAdClick() {
         didOpen: async () => {
             let buttonAdsConfirm = Swal.getConfirmButton();
             await buttonAdsConfirm.setAttribute("disabled", true);
-            if(locCountry&&locCountry!="ID") {
-                let elJsBannerParam=document.createElement("script");
-                await elJsBannerParam.setAttribute("type","text/javascript");
-                elJsBannerParam.innerHTML=`
-                atOptions = {
-                    'key' : '5b9bb815a5d7f812bbd4762237899e19',
-                    'format' : 'iframe',
-                    'height' : 250,
-                    'width' : 300,
-                    'params' : {}
-                };
-            `;
-            await document.getElementById("container-ads-300-250").append(elJsBannerParam);
-            let elJsBannerPopup=document.createElement("script");
-            await elJsBannerPopup.setAttribute("type","text/javascript");
-            await elJsBannerPopup.setAttribute("src","https://tyranhorrid.com/5b/9b/b8/5b9bb815a5d7f812bbd4762237899e19.js");
-            await document.getElementById("container-ads-300-250").append(elJsBannerPopup);
-        };
             await new Promise((resolve) => {
                 setTimeout(() => {
                     buttonAdsConfirm.removeAttribute("disabled");
-                    buttonAdsConfirm.innerText = "Tutup Iklan";
+                    buttonAdsConfirm.innerText = "Cek Info";
                     resolve();
-                }, 1000);
+                }, 2000);
             });
         },
-        willClose:() => {
-                if(statusClickSwallClose==false) {
-                    if(locCountry&&locCountry=="ID") {
-                        window.open(urlOpenClick,"_blank");
-                        window.scrollTo({
-                            top:0
-                        });
-                        setTimeout(() => {
-                            loadNextAds();
-                        }, 1000);
-                    } else {
-                        window.open(urlOpenClick2,"_blank");
-                    };
+        willClose: () => {
+            if (statusClickSwallClose == false) {
+                if (locCountry && locCountry == "ID") {
+                    window.open(urlOpenClick, "_blank");
+                } else {
+                    window.open(urlOpenClick2, "_blank");
                 };
-            }
-        });
-    }();
-};
+            };
+            loadNextAds();
+        }
+    });
+})();
